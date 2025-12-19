a23a3d22b5c26087667a90a6d52a63b9411a738f
galsiegel <gal4040@gmail.com>
2025-12-19 16:51:22 +0200

    starting fresh

--- classes/degree_registry.py
+++ classes/degree_registry.py
@@ -1,48 +1,12 @@
 """
 Degree registry - factory functions to create degrees with their buckets.
+A class that is used for super admin
 """
 
-from typing import Dict, Callable
-from .degree import Degree
-from .faculty import Faculty
-from .programs import Program_bucket, BucketType
-
-
-# Registry of degree creation functions
-DEGREE_REGISTRY: Dict[str, Callable[[], Degree]] = {}
-
-
-def register_degree(degree_id: str):
-    """Decorator to register a degree creation function."""
-    def decorator(func):
-        DEGREE_REGISTRY[degree_id] = func
-        return func
-    return decorator
-
-
-def get_degree(degree_id: str) -> Degree:
-    """
-    Get a degree by its ID.
-    
-    Args:
-        degree_id: The degree identifier
-        
-    Returns:
-        Degree object
-        
-    Raises:
-        KeyError: If degree_id is not found
-    """
-    if degree_id not in DEGREE_REGISTRY:
-        available = ", ".join(sorted(DEGREE_REGISTRY.keys()))
-        raise KeyError(
-            f"Degree '{degree_id}' not found. Available degrees: {available}"
-        )
-    return DEGREE_REGISTRY[degree_id]()
 
 
 # Example degree creation functions
-# TODO: Replace these with actual degree definitions based on your data
+# TODO: Replace these with actual degree definitions based on data
 
 @register_degree("ELECTRICAL_ENGINEERING")
 def create_electrical_engineering() -> Degree:
